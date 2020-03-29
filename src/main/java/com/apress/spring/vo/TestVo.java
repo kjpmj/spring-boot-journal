6aa6b28bb8a4168164889e5f9d45338772f6f743
MJ <kjpmj@naver.com>
2020-03-30 01:34:22 +0900

    2020-03-30

---
+++ src/main/java/com/apress/spring/vo/TestVo.java
@@ -0,0 +1,20 @@
+package com.apress.spring.vo;
+
+public class TestVo {
+	private String className;
+	private String method;
+	
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getMethod() {
+		return method;
+	}
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	
+}
--- src/main/java/com/apress/spring/web/JournalController.java
+++ src/main/java/com/apress/spring/web/JournalController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.apress.spring.domain.Journal;
 import com.apress.spring.repository.JournalRepository;
+import com.apress.spring.vo.TestVo;
 
 @Controller
 public class JournalController {
@@ -18,6 +22,19 @@ public class JournalController {
 	
 	@RequestMapping(value="/journal") 
 	public @ResponseBody List<Journal> getJournal() {
+		System.out.println("GET Journal");
 		return repo.findAll();
+	}
+	
+	@RequestMapping(value="/") 
+	public @ResponseBody String getMsg(@RequestBody TestVo vo) {
+		System.out.println("call getMsg");
+		return "받은 파라미터들: " + vo.getClassName() + "  " + vo.getMethod();
+	}
+	
+	@RequestMapping(value="/testvo") 
+	public @ResponseBody TestVo getTestVo(@RequestBody TestVo vo) {
+		System.out.println("call getTestVo");
+		return vo;
 	}
 }
